91885c0547a9752b795f7ae14bb63f541358c12d
UziAlam <u.alam@live.com.au>
2025-11-08 16:12:37 +0100

    Completed

--- src/presidents.js
+++ src/presidents.js
@@ -416,30 +416,40 @@ const presidents = [
 ];
 
 
-
-
 // Iteration 1 | Names of All Presidents - `map()`
-function getNames(presidentsArr) {}
-
-
+function getNames(presidentsArr) {
+  return presidentsArr.map(presidents => presidents.name)  
+}
+console.log(getNames(presidents))
 
 
 // Iteration 2 | Democratic Presidents - `filter()`
-function getDemocraticPresidents(presidentsArr) {}
+function getDemocraticPresidents(presidentsArr) {
+  return presidentsArr.filter(presidents => presidents.party === "Democratic");
+}
+console.log(getDemocraticPresidents(presidents))
+
 
 
 
 
 // Iteration 3 | Count Years in Office - reduce()
-function  countYearsInOffice(presidentsArr) {}
+function  countYearsInOffice(presidentsArr) {
+return presidentsArr.reduce((totalyears, president) => {
+    if (president.leftOffice !== null)  {
+      return totalyears += president.leftOffice - president.tookOffice
+    }
+    return totalyears
+}, 0)   
+}
 
-
+console.log(countYearsInOffice(presidents))
 
 
 // Iteration 4 | Sort Presidents by Birth Year - `sort()`
-function sortPresidentsByBirthYear(presidentsArr) {}
-
-
+ function sortPresidentsByBirthYear(presidentsArr) {
+   return presidentsArr.sort((a, b) => a.birthYear - b.deathYear);
+}
 
 
 // Bonus: Iteration 5 | Age at Inauguration - `map()`
